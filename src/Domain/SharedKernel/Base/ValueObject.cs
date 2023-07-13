7b12f228e22fc9d93963d0c19abd37d000c09ac0
Son Quang Pham (SM) <sonpham@deloitte.ca>
2023-07-13 10:21:05 +0700

    Update ValueObject

--- src/Domain/SharedKernel/Base/ValueObject.cs
+++ src/Domain/SharedKernel/Base/ValueObject.cs
@@ -50,13 +50,13 @@ public abstract class ValueObject : IComparable, IComparable<ValueObject>
         Type thisType = this.GetUnproxiedType();
         Type otherType = other.GetUnproxiedType();
         if (thisType != otherType)
-            return string.Compare($"{thisType}", $"{otherType}", StringComparison.Ordinal);
+            return string.Compare(thisType.Name, otherType.Name, StringComparison.Ordinal);
 
         return
             GetEqualityComponents().Zip(
                 other.GetEqualityComponents(),
-                (left, rigth) =>
-                    left?.CompareTo(rigth) ?? (rigth is null ? 0 : -1))
+                (left, right) =>
+                    left?.CompareTo(right) ?? (right is null ? 0 : -1))
             .FirstOrDefault(cmp => cmp != 0);
     }
 
