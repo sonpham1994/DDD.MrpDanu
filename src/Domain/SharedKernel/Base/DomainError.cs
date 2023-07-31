2afb9c8b07193677418b0c8996c5bd4e801e6e01
SonPham <phamquangson1994@gmail.com>
2023-07-31 21:49:43 +0700

    Add some good practice for struct

--- src/Domain/SharedKernel/Base/DomainError.cs
+++ src/Domain/SharedKernel/Base/DomainError.cs
@@ -1,5 +1,18 @@
 namespace Domain.SharedKernel.Base;
 
+/*
+ * Good design for struct:
+ *  ✔️ CONSIDER defining a struct instead of a class if instances of the type are small and commonly short-lived or are commonly embedded in other objects.
+ *  ❌ AVOID defining a struct unless the type has all of the following characteristics:
+ *      - It logically represents a single value, similar to primitive types (int, double, etc.).
+ *      - It has an instance size under 16 bytes.
+ *      - It is immutable.
+ *      - It will not have to be boxed frequently.
+ *
+ * https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/struct?redirectedfrom=MSDN
+ * https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/choosing-between-class-and-struct?redirectedfrom=MSDN
+ * https://stackoverflow.com/questions/34353330/c-sharp-struct-vs-class-performace-design-focus
+ */
 public readonly struct DomainError : IEquatable<DomainError>
 {
     internal static DomainError Empty => new(string.Empty, string.Empty);
