e8d0e11d87aaa0263f90106fd906712b9cb815af
SonPham <phamquangson1994@gmail.com>
2023-12-16 17:37:18 +0700

    Add migration UniqueCodeForMaterial

---
+++ src/External/Infrastructure/Persistence/Migrations/20231216103643_AddUniquerForMaterialCode.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniquerForMaterialCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Material_Code",
+                table: "Material",
+                column: "Code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Material_Code",
+                table: "Material");
+        }
+    }
+}
