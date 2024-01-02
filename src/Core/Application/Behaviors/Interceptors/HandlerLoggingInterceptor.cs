ea9fb2d0178bdae25ec5f490b840cf1702a241ed
SonPham <phamquangson1994@gmail.com>
2024-01-02 22:23:48 +0700

    check GC is full

--- src/Core/Application/Behaviors/Interceptors/HandlerLoggingInterceptor.cs
+++ src/Core/Application/Behaviors/Interceptors/HandlerLoggingInterceptor.cs
@@ -13,18 +13,22 @@ internal sealed class HandlerLoggingInterceptor<TRequest, TResponse> : IPipeline
     where TResponse : IResult
 {
     private readonly ILogger<HandlerLoggingInterceptor<TRequest, TResponse>> _logger;
+    private readonly ILogger<GCEventListener> _loggerGCEventListener;
     private readonly IRequestHandler<TRequest, TResponse> _requestHandler;
 
     public HandlerLoggingInterceptor(IRequestHandler<TRequest, TResponse> requestHandler,
-        ILogger<HandlerLoggingInterceptor<TRequest, TResponse>> logger)
+        ILogger<HandlerLoggingInterceptor<TRequest, TResponse>> logger,
+        ILogger<GCEventListener> loggerGCEventListener)
     {
         _requestHandler = requestHandler;
         _logger = logger;
+        _loggerGCEventListener = loggerGCEventListener;
     }
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
         CancellationToken cancellationToken)
     {
+        using var gcEventListener = new GCEventListener(_loggerGCEventListener);
         var handlerName = _requestHandler.GetType().Name;
         var requestTypeName = typeof(TRequest).Name;
         
@@ -32,7 +36,6 @@ internal sealed class HandlerLoggingInterceptor<TRequest, TResponse> : IPipeline
         var start = Stopwatch.GetTimestamp();
         var response = await next();
         var delta = Stopwatch.GetElapsedTime(start);
-        
         if (response.IsFailure)
         {
             _logger.HandleFailure(handlerName, requestTypeName, response.Error, delta.TotalMicroseconds);
---
+++ src/Core/Application/GCEventListener.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics.Tracing;
+using Microsoft.Extensions.Logging;
+
+namespace Application;
+
+internal sealed class GCEventListener : EventListener
+{
+    private readonly ILogger<GCEventListener> _logger;
+
+    public GCEventListener(ILogger<GCEventListener> logger)
+    {
+        _logger = logger;
+    }
+    protected override void OnEventSourceCreated(EventSource eventSource)
+    {
+        if (eventSource.Name.Equals("System.Runtime"))
+        {
+            EnableEvents(eventSource, EventLevel.Informational, (EventKeywords)(-1));
+        }
+    }
+
+    protected override void OnEventWritten(EventWrittenEventArgs eventData)
+    {
+        // Check if the event is related to GC
+        if (!string.IsNullOrEmpty(eventData.EventName) && eventData.EventName.Contains("GC"))
+        {
+            _logger.LogWarning("GC Event: {EventName}, Message: {Message}, Time: {@Time}, ", eventData.EventName, eventData.Message, eventData.TimeStamp);
+        }
+    }
+}
